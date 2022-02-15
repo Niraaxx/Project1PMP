3bffd77532274421e87a6ce6265057ec26b858e7
Niraaxx <60941711+Niraaxx@users.noreply.github.com>
2022-02-15 16:19:07 +0100

    Update testParser.cc

--- testParser.cc
+++ testParser.cc
@@ -4,6 +4,7 @@
 #include "Parser.h"
 
 using namespace op;
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
@@ -50,8 +51,6 @@ TEST(OptionTest,OptionTestGetNamesError){
   std::vector<std::string> test = opt.getNames();
   EXPECT_NE("h",test[1]);
   EXPECT_NE("c",test[0]);
-
-
 }
 
 TEST(OptionTest,OptionTestGetValue){
@@ -70,19 +69,6 @@ TEST(OptionTest,OptionTestGetNoValue2){
   Option opt=Option("command");
   opt.setDefaultValue("");
   EXPECT_THROW({opt.getValue();},std::runtime_error);
-}
-
-TEST(OptionTest,OptionTestExpectValueTrue){
-  Option opt=Option("command");
-  opt.setDefaultValue("c");
-  EXPECT_TRUE(opt.expectValue());
-
-}
-
-TEST(OptionTest,OptionTestExpectValueFalse){
-  Option opt=Option("command");
-  EXPECT_FALSE(opt.expectValue());
-
 }
 
 TEST(OptionTest,OptionTestSetDefaultValue){
@@ -235,33 +221,141 @@ TEST(OptionTest,OptionTestOperatorBoolFalse){
   Option opt=Option("command");
   EXPECT_FALSE(opt.operator bool());
 }
-TEST(ParserTest,ParserTestContructor){
-//   Parser parser;
-//   parser("azert");
-//   const char* argv[] = {
-// " command " ,
-// " --option " ,
-// " bar " ,
-// " -a " ,
-// " pos1 " ,
-// " pos2 "
-// };
-//   parser.parseCommandLine(6,argv);
-//   EXPECT_FALSE(parser("azert"));
-  
 
+
+/*PARSER TEST*/ 
+
+
+
+TEST(ParserTest,ParserTestContructorGoodByAlias){
+  Parser p;
+  EXPECT_EQ(p("h").getNames()[0], "help");
+  EXPECT_EQ(p("h").getNames()[1], "h");
 }
 
-TEST(ParserTest,ParserTestparserCommandLine){
-
+TEST(ParserTest,ParserTestContructorGood){
+  Parser p;
+  EXPECT_EQ(p("help").getNames()[0], "help");
+  EXPECT_EQ(p("help").getNames()[1], "h");
 }
 
-TEST(ParserTest,ParserTestgetPositionalArgumentCount){
-
+TEST(ParserTest,ParserTestOperatorParWithOutAliasName){
+  Parser p;
+  p("OPTION");
+  EXPECT_EQ(p("OPTION").getNames()[0], "OPTION");
+  EXPECT_EQ(p("OPTION").getNames().size(),1);
 }
-TEST(ParserTest,ParserTestOperator1){
 
+TEST(ParserTest,ParserTestOperatorParWithOutAliasSize){
+  Parser p;
+  p("OPTION");
+  EXPECT_EQ(p("OPTION").getNames().size(),1);
 }
+
+TEST(ParserTest,ParserTestOperatorParWithAliasName){
+  Parser p;
+  p("OPTION").addAlias("SUPER");
+
+  EXPECT_EQ(p("OPTION").getNames()[0], "OPTION");
+  EXPECT_EQ(p("OPTION").getNames()[1], "SUPER");
+  EXPECT_EQ(p("SUPER").getNames()[0], "OPTION");
+  EXPECT_EQ(p("SUPER").getNames()[1], "SUPER");
+}
+
+TEST(ParserTest,ParserTestOperatorParWithAliasSize){
+  Parser p;
+  p("OPTION").addAlias("SUPER");
+
+  EXPECT_EQ(p("OPTION").getNames().size(),2);
+  EXPECT_EQ(p("SUPER").getNames().size(),2);
+}
+
+TEST(ParserTest,ParserTestPrintHelpOnly){
+  Parser p;
+  p.printHelp(std::cout);
+}
+
+TEST(ParserTest,ParserTestPrintHelpWithOneOption){
+  Parser p;
+  p("OneOPTION");
+  p.printHelp(std::cout);
+}
+
+TEST(ParserTest,ParserTestPrintHelpWithManyOptions){
+  Parser p;
+  p("OneOPTION");
+  p("TwoOPTION");
+  p("ThreeOPTION");
+  p.printHelp(std::cout);
+}
+
+TEST(ParserTest,ParserTestPrintHelpWithManyOptionsAndOneAlias){
+  Parser p;
+  p("OneOPTION");
+  p("OneOPTION").addAlias("p");
+  p("TwoOPTION");
+  p("ThreeOPTION");
+  p.printHelp(std::cout);
+}
+
+TEST(ParserTest,ParserTestPrintHelpWithManyOptionsAndManyAlias){
+  Parser p;
+  p("OneOPTION");
+  p("OneOPTION").addAlias("p");
+  p("OneOPTION").addAlias("o");
+  p("TwoOPTION");
+  p("ThreeOPTION");
+  p.printHelp(std::cout);
+}
+
+/*TEST(ParserTest,ParserTestGetPositionalArgumentCountGood){
+  const char* argv[] = {
+  (char *)"command ",
+  (char *)"--option ",
+  (char *)"bar ",
+  (char *)"pos2 "
+  };
+  Parser p;
+  p("option").hasValue().setDefaultValue("foo");
+  p.parseCommandLine(sizeof(argv),argv);
+  auto count = p.getPositionalArgumentCount();
+  EXPECT_EQ(count,1);
+}
+
+TEST(ParserTest,ParserTestGetPositionalArgumentCountManyGood){
+  const char* argv[] = {
+  (char *)"command ",
+  (char *)"--option ",
+  (char *)"bar ",
+  (char *)"-a",
+  (char *)"pos1",
+  (char *)"pos2 "
+  };
+  Parser p;
+  p("option").hasValue().setDefaultValue("foo");
+  p("alias").addAlias("a");
+  p.parseCommandLine(sizeof(argv),argv);
+  auto count = p.getPositionalArgumentCount();
+  EXPECT_EQ(count,2);
+}
+
+TEST(ParserTest,ParserTestGetPositionalArgumentCountNotGood){
+  const char* argv[] = {
+  (char *)"command ",
+  (char *)"--option ",
+  (char *)"bar ",
+  (char *)"-a",
+  (char *)"pos1",
+  (char *)"pos2 "
+  };
+  Parser p;
+  p("option").hasValue().setDefaultValue("foo");
+  p("alias").addAlias("a");
+  p.parseCommandLine(sizeof(argv),argv);
+  auto count = p.getPositionalArgumentCount();
+  EXPECT_EQ(count,1);
+}
+
 TEST(ParserTest,ParserTestOperator2){
 
-}
+}*/
