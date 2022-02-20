5e711c2dc62782e8a8ecc9a0161ac1b89591a776
didi512 <56636270+didi512@users.noreply.github.com>
2022-02-20 20:46:02 +0100

    Update Parser.cc

--- Parser.cc
+++ Parser.cc
@@ -4,47 +4,66 @@
 #include <stdexcept>
 
 namespace op {
-
+  //Constructor of the Parser
   Parser::Parser(){
     Option helpOpt = Option("help");
     helpOpt.addAlias("h");
     options.push_back(helpOpt);
   }
-
+  //read the option on the line command
   void Parser::parseCommandLine(int argc, const char* const argv[]) {
-    for(int i =1; i<=argc;i++){
+    //browse the line command
+    for(int i =0; i<argc;i++){  
       std::string opt = (std::string) argv[i];
-      if((strcmp(&opt.at(0),"-") == 0) && (strcmp(&opt.at(1),"-") == 0)
-      /*(strncmp(&argv[i][0],"-",1) == 0) || (strncmp(&argv[i][1],"-",1) == 0)*/){
+      std::string opt1=opt.substr(0,1);
+      std::string opt2=opt.substr(1,1);
+      //check if there are two -
+      if((opt1.compare("-")==0)&&(opt2.compare("-")==0)){
+        //browse the option
         for(int j=0; j< options.size();j++){
-          if(options[i].operator!=(opt.substr(opt.find("-")+2))){
+          //if it's a new option
+          if(options[j].operator!=(opt.substr(opt.find("-")+2))){   
             Option optObj = operator()(opt);
+
+            //check if this option expect a value 
             if(optObj.expectValue()){
               optObj.operator=(argv[i+1]);
               i++;
             }
+            //parsed the option and then add the option in the list of option
             optObj.parsed();
             options.push_back(opt);
           }else{
             throw std::runtime_error("Not an option");
           }
         }
-      }else if((strcmp(&opt.at(0),"-") == 0)){
+      //check if there is a - 
+      }else if(opt1.compare("-")==0){
+
+        //browse the list of option
         for(int j=0; j< options.size();j++){
-          if(options[i].operator!=(opt.substr(opt.find("-")+1))){
+
+          //if the option doesn't exit in the list
+          if(options[j].operator!=(opt.substr(opt.find("-")+1))){
             Option optObj = operator()(opt);
+
+            //check if the option expect a value
             if(optObj.expectValue()){
               optObj.operator=(argv[i+1]);
               i++;
             }
+            //parsed and add the option in the list of options
             optObj.parsed();
             options.push_back(opt);
           }else{
             throw std::runtime_error("Not an option");
           }
         }
-      }
+       }
+       //if there is no - it is a value 
       else{
+        //std::cout << "premier element de option "<<opt.substr(0,1)<<" deuxieme element de l'option "<<opt.substr(1,1) <<"\n";
+
         argPositionnels.push_back(opt);
       }
     }
@@ -56,7 +75,7 @@ namespace op {
       }
     }
   }
-
+  //print the help 
   void Parser::printHelp(std::ostream& stream) const {
     stream << "Usage:\n";
     for(int i =1; i< options.size() ;i++){
@@ -70,11 +89,11 @@ namespace op {
     }
     stream << "\t--"<< options[0].getNames().at(0) <<" |-"<< options[0].getNames().at(1) <<"\n";
   }
-
+  //it's a function to know the number of positional arguments
   std::size_t Parser::getPositionalArgumentCount() const {
     return argPositionnels.size();
   }
-
+  //it's to get the option by it's name
   Option& Parser::operator()(const std::string& name) {
     for(auto &option : this->options){
       if(option == name){
@@ -84,7 +103,7 @@ namespace op {
     options.emplace_back(name);
     return options.back();
   }
-
+  //to determinate if the positional argument exist 
   const std::string& Parser::operator[](std::size_t i) const {
     if(i >= getPositionalArgumentCount()) throw std::out_of_range("Positional argument doesn't exist");
     return argPositionnels[i];
